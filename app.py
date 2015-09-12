78f1146338dcfcca3c37bdd16e4528071181b982
nmill128 <nmill128@gmail.com>
2015-09-12 04:18:11 -0400

    Now with fewer imports

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-import os
-import json
-import bson.json_util
+
 import flask
 from flask import Flask
 from flask.ext.pymongo import PyMongo
-from datetime import datetime,timedelta
-import sendgrid
-import urllib,urllib2
+
 
 app = Flask(__name__)
 # client = MongoClient()
