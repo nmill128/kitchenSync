7f3f66f14ecb98222470bbd687dfc32f9801628c
nmill128 <nmill128@gmail.com>
2015-09-12 11:37:44 -0400

    Now actually though

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def getUsers():
 	food_id = record["Food_ID"]
 	user = record["User"]
 	Date_Used = record[Date_Used]
-	jsonstr {"ID": obj_id, "Food_ID":food_id,"User":user, "Date-Used":Date_Used}
+	jsonstr ={"ID": obj_id, "Food_ID":food_id,"User":user, "Date-Used":Date_Used}
 	return json.dumps(jsonstr)
 # @app.route('/Trial')
 # def getTrial():
