87d8ab3c7acac87ddedf10b5c749f57c976e8d39
nmill128 <nmill128@gmail.com>
2015-09-12 04:14:46 -0400

    Here we go

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
+import os
+import json
+import bson.json_util
+import flask
 from flask import Flask
+from flask.ext.pymongo import PyMongo
+from datetime import datetime,timedelta
+import sendgrid
+import urllib,urllib2
 
 app = Flask(__name__)
+client = MongoClient()
+users = client.Users_database
+
 
 @app.route('/')
 def index():
     return "<h1> This flask app is running!<h1>"
+
+@app.route('/Users')
+def getUsers():
+	return "Hi"
 
 if __name__=='__main__':
     app.run(port=8000)
