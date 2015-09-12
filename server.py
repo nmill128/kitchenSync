4cd71ae152855463170a26ec99730e3ad73ae18a
nmill128 <nmill128@gmail.com>
2015-09-12 00:13:10 -0400

    memories coming back

--- server.py
+++ server.py
@@ -14,7 +14,11 @@ app = Flask(__name__, static_url_path='/')
 @app.route('/')
 def root(): 
 
-	
+@app.route('/reminder')
+def remember:
+	return 'Remember, Remember'
+
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=int(port), debug=True)
+
