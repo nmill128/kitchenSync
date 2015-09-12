b872cdef5156b0b817224fa580ac4fab93cefe2e
nmill128 <nmill128@gmail.com>
2015-09-12 11:35:29 -0400

    Now with JSON

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import flask
 import pymongo
+import json
+import bson.json_util
 from flask import Flask
 from pymongo import MongoClient
 
@@ -14,6 +16,13 @@ except pymongo.errors.ConnectionFailure, e:
    print "Could not connect to MongoDB: %s" % e 
 db = client.test
 
+class DateTimeEncoder(json.JSONEncoder):
+  def default(self, obj):
+      if isinstance(obj, datetime):
+          encoded_object = list(obj.timetuple())[0:6]
+      else:
+          encoded_object =json.JSONEncoder.default(self, obj)
+      return encoded_object
 
 @app.route('/')
 def index():
@@ -26,18 +35,23 @@ def getUsers():
 	#client.users.insert({"name":"Yeomans"})
 	# return client.users.find_one()
 	# print client.database_names()
-	print "Users"
-	print db.users
-	print "Restock"
-	print db.restock.find_one()
-	print "Fridge"
-	print db.fridge
-	print "Stock"
-	print db.stock
+	# print "Users"
+	# print db.users
+	# print "Restock"
+	# print db.restock.find_one()
+	# print "Fridge"
+	# print db.fridge
+	# print "Stock"
+	# print db.stock
 
 	 
 	record = db.restock.find_one()
-	return record["Food_ID"]
+	obj_id = record["Object ID"]
+	food_id = record["Food_ID"]
+	user = record["User"]
+	Date_Used = record[Date_Used]
+	jsonstr {"ID": obj_id, "Food_ID":food_id,"User":user, "Date-Used":Date_Used}
+	return json.dumps(jsonstr)
 # @app.route('/Trial')
 # def getTrial():
 # 	return "hi"
