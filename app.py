908870a2f375fe005f248d7b3562d84a77520d92
nmill128 <nmill128@gmail.com>
2015-09-12 20:56:03 -0400

    Now for chris lai

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def index():
 	#message = client.sms.messages.create(to=+17038557270, from_=+17038103574,body="Hello there!")
 	return "<h1>Hi Ben!<h1>"
 
-@app.route('/dashboard')
+@app.route('/<username>/dashboard')
 def userDash(username):
 	record = db.users.find_one({"username":username})
 	print record
