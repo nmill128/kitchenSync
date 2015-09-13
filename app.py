be4a20e36ef79bb34b3637489351564a3b9ed3c9
nmill128 <nmill128@gmail.com>
2015-09-12 20:38:01 -0400

    No messaging yeomasn

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def getDate(dt):
 
 @app.route('/')
 def index():
-	message = client.sms.messages.create(to=+17038557270, from_=+17038103574,body="Hello there!")
+	#message = client.sms.messages.create(to=+17038557270, from_=+17038103574,body="Hello there!")
 	return "<h1>Hi Ben!<h1>"
 
 @app.route('/<username>/dashboard')
