c281c2e8bf70e249b4cde03cf75725ad8255f2cc
nmill128 <nmill128@gmail.com>
2015-09-13 05:24:25 -0400

    Proper use

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def requestFood(username):
 		print friend
 		f = db.users.find_one({"username":friend})
 		number = "1"+f["Phone"]
-		message = client.sms.messages.create(to=+long(number), from_=+17038103574,body="Hello!\n Your friend " + record["Name"]+ " needs " + foodName)
+		message = client.sms.messages.create(to=+long(number), from_=+17038103574,body="Hello!\n Your friend " + record["Name"]+ " needs " + name)
 
 @app.route('/twilio/sms', methods = ["POST"])
 def response():
