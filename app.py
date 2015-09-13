490d793168f74d4617d97de89e07c23f888f82bd
nmill128 <nmill128@gmail.com>
2015-09-13 05:21:55 -0400

    Phone is capitalized

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def requestFood(username):
 	print friends
 	for friend in friends:
 		f = db.user.find_one({"username":friend})
-		number = "1"+f["phone"]
+		number = "1"+f["Phone"]
 		message = client.sms.messages.create(to=+long(number), from_=+17038103574,body="Hello!\n Your friend " + record["name"]+ " needs " + foodName)
 
 @app.route('/twilio/sms', methods = ["POST"])
